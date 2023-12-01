8f5d706214e157b11a1aa7f9177c238bd83b4a4e
GavinOP <HaoxiangZhang@link.cuhk.edu.hk>
2023-12-01 14:57:31 +0800

    update main

--- phase2_code_implementation/Main.java
+++ phase2_code_implementation/Main.java
@@ -3,25 +3,28 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main extends Page {
-    public static void main(String[] args) throws SQLException {
-        String driverName = "com.mysql.jdbc.Driver";
-        String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db60?autoReconnect=true&useSSL=false";
-        String userName = "Group26";
-        String userPwd = "CSCI3170";
-        Connection conn;
 
+    // Database credentials
+    private static final String dbAddress = "jdbc:mysql://projgw.cse.cuhk.edu.hk:2633/db60?autoReconnect=true&useSSL=false";
+    private static final String dbUsername = "Group60";
+    private static final String dbPassword = "CSCI3170";
+
+    public static Connection connectToMySQL() {
+        Connection con = null;
         try {
-            Class.forName(driverName);
-
-            conn = DriverManager.getConnection(dbAddress, userName, userPwd);
-
-        } catch(Exception e) {
-            e.printStackTrace();
-
-            System.out.println("Connection failed!");
-
-            return;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(dbAddress, dbUsername, dbPassword);
+        } catch (ClassNotFoundException e) {
+            System.out.println("[Error]: Java MySQL DB Driver not found!!");
+            System.exit(0);
+        } catch (SQLException e) {
+            System.out.println(e);
         }
+        return con;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = connectToMySQL();
 
         boolean running = true;
         while (running) {
@@ -32,8 +35,10 @@ public class Main extends Page {
                     AdministratorPage.start(conn);
                     break;
                 case 2:
+                    // Add code for case 2
                     break;
                 case 3:
+                    // Add code for case 3
                     break;
                 default:
                     System.out.println("Bye Bye!");
@@ -41,6 +46,5 @@ public class Main extends Page {
                     break;
             }
         }
-
     }
 }
