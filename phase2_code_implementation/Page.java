d05a6b877dc98ea2041105a28e8fff2fe45bba0e
GavinOP <HaoxiangZhang@link.cuhk.edu.hk>
2023-12-01 15:14:25 +0800

    Update Page.java

--- phase2_code_implementation/Page.java
+++ phase2_code_implementation/Page.java
@@ -2,48 +2,46 @@ import java.util.Scanner;
 
 public class Page {
 
+    // public static int takeChoiceInput(int min, int max) {
+    //     String input;
+    //     Scanner sc = new Scanner(System.in);
+    //     while (true) {
+    //         System.out.print("Enter Your Choice: ");
+    //         input = sc.next();
+
+    //         if (!input.matches(".*[a-zA-Z]+.*") && Integer.parseInt(input) >= min && Integer.parseInt(input) <= max) {
+    //             return Integer.parseInt(input);
+    //         }
+    //         else {
+    //             System.out.printf("\nInvalid input\n");
+    //         }
+    //     }
+    // }
+
     public static int takeChoiceInput(int min, int max) {
-        String input;
-        Scanner sc = new Scanner(System.in);
-        try( Scanner sc = new Scanner(System.in)) {
+        try (Scanner sc = new Scanner(System.in)) {
             while (true) {
                 System.out.print("Enter Your Choice: ");
-                input = sc.next();
 
-                if (!input.matches(".*[a-zA-Z]+.*") && Integer.parseInt(input) >= min && Integer.parseInt(input) <= max) {
-                    return Integer.parseInt(input);
-                }
-                else {
-                    System.out.printf("\nInvalid input\n");
+                String inputLine = sc.next();
+
+                if (!inputLine.isEmpty()) {
+                    try {
+                        int choice = Integer.parseInt(inputLine);
+                        if (choice >= min && choice <= max) {
+                            return choice;
+                        } else {
+                            System.out.printf("\nInvalid input. Please enter a number between %d and %d.\n", min, max);
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("\nInvalid input. Please enter a valid integer.\n");
+                    }
+                } else {
+                    System.out.println("\nNo input detected. Please try again.\n");
                 }
             }
         }
     }
-
-    // public static int takeChoiceInput(int min, int max) {
-    //     try (Scanner sc = new Scanner(System.in)) {
-    //         while (true) {
-    //             System.out.print("Enter Your Choice: ");
-
-    //             String inputLine = sc.nextLine().trim();
-
-    //             if (!inputLine.isEmpty()) {
-    //                 try {
-    //                     int choice = Integer.parseInt(inputLine);
-    //                     if (choice >= min && choice <= max) {
-    //                         return choice;
-    //                     } else {
-    //                         System.out.printf("\nInvalid input. Please enter a number between %d and %d.\n", min, max);
-    //                     }
-    //                 } catch (NumberFormatException e) {
-    //                     System.out.println("\nInvalid input. Please enter a valid integer.\n");
-    //                 }
-    //             } else {
-    //                 System.out.println("\nNo input detected. Please try again.\n");
-    //             }
-    //         }
-    //     }
-    // }
 
 
 
